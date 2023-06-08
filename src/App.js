585b0f950ab86c3abf71d7612b7578101c7a28c1
Pawan Kumar Siwakoti <pksiwako@deakin.edu.au>
2023-06-08 11:11:36 +1000

    admin register and login UI

--- src/App.js
+++ src/App.js
@@ -1,22 +1,19 @@
-import logo from './logo.svg';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Login from './pages/signup-signin/Login';
+import SignUp from './pages/signup-signin/SignUp';
+import Home from './pages/signup-signin/Home';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="">
+        <Routes>
+          <Route path='/' element={<Home />}></Route>
+          <Route path='/signin' element={<Login />}></Route>
+          <Route path='/signup' element={<SignUp />}></Route>
+        </Routes>
+       
       </header>
     </div>
   );
